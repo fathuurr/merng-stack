2f59fafb01eae6c0949feb99155655249433978b
Fathurrahman <nzihtlr926@gmail.com>
2020-10-22 11:47:05 +0700

    created post

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,48 @@
 const User = require('../../models/User');
 const { SECRET_KEY } = require('../../config');
-const { validateRegisterInput } = require('../../util/validators');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../../util/validators');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { UserInputError } = require('apollo-server');
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = 'User Not Found';
+        throw new UserInputError('Wrong Credentials', { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong Credentials';
+        throw new UserInputError('Wrong Credentials', { errors });
+      }
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -41,15 +77,7 @@ module.exports = {
       });
 
       const res = await newUser.save();
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      );
+      const token = generateToken(res);
       return {
         ...res._doc,
         id: res._id,
